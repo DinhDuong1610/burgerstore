a06b9caa1acb09e48afccf6117afbc857aba358b
Dinh Duong <jenny.180820@gmail.com>
2024-11-25 15:33:34 +0700

    done Model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function order_items() {
+        return $this->hasMany(Order_item::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function order_items() {
+        return $this->hasMany(Order_item::class);
+    }
+
+    public function carts() {
+        return $this->hasMany(Cart::class);
+    }
 }
